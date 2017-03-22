097128f40d7e43c415706ca78aa80674393c4fae
murjax <murjax@gmail.com>
2017-03-22 17:14:28 -0400

    Remove a few lines of code

--- ruby/github_repo_cloner.rb
+++ ruby/github_repo_cloner.rb
@@ -2,20 +2,17 @@ require 'net/https'
 require 'json'
 require 'git'
 require 'net/ping'
+
 puts "Please enter your Github account name"
 STDOUT.flush
 account_name = gets.chomp
 
-if Net::Ping::HTTP.new("http://www.google.com/index.html").ping?
-	user_response = Net::HTTP.get(URI.parse("https://api.github.com/users/#{account_name}"))
-	repo_response = Net::HTTP.get(URI.parse("https://api.github.com/users/#{account_name}/repos"))
-	repositories = JSON.parse(user_response)
-
-	if repositories["message"]
+if Net::Ping::HTTP.new("https://api.github.com").ping?
+	user_response = JSON.parse(Net::HTTP.get(URI.parse("https://api.github.com/users/#{account_name}")))
+	if user_response["message"]
 		puts "Account does not exist"
 	else
-		repositories = JSON.parse(repo_response)
-
+		repositories = JSON.parse(Net::HTTP.get(URI.parse("https://api.github.com/users/#{account_name}/repos")))
 		if repositories.count > 0
 			repositories.each do |repo|
 				git = Git.clone(repo["clone_url"], repo["name"], :path => "my_repositories")
@@ -25,7 +22,6 @@ if Net::Ping::HTTP.new("http://www.google.com/index.html").ping?
 		else
 			puts "No repositories found at this account"
 		end
-
 	end
 else 
 	puts "network error"
