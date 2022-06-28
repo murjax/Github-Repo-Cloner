607917cbf82e0865b3e4a3fac3808ff2baf54af8
kemptosa <99417853+kemptosa@users.noreply.github.com>
2022-06-28 19:42:06 -0400

    python3 syntax + prevent cloning of existing repos

--- python/github_repo_cloner.py
+++ python/github_repo_cloner.py
@@ -3,12 +3,14 @@ import git
 import requests
 
 from git import Repo
-username = raw_input("Please enter your Github account name: ")
-response = requests.get("https://api.github.com/users/" + username + "/repos")
+username = input("Please enter your Github account name: ")
+response = requests.get("https://api.github.com/users/" + username + "/repos?per_page=100")
 repositories =  response.json()
+current_repos = os.listdir("./my_repositories/")
 
 for repo in repositories:
-	command = git.Git().clone(repo["clone_url"], "my_repositories/" + repo["name"], recursive=True)
-	r = Repo("my_repositories/" + repo["name"])
-	r.create_remote('originate', repo["clone_url"])
-	print command
+    if repo["name"] not in current_repos:
+        command = git.Git().clone(repo["clone_url"], "my_repositories/" + repo["name"], recursive=True)
+        r = Repo("my_repositories/" + repo["name"])
+        r.create_remote('originate', repo["clone_url"])
+        print(repo["name"])
