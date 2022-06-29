5b83e5cf293da2dba3a54c0572c7714c441055d1
kemptosa <99417853+kemptosa@users.noreply.github.com>
2022-06-28 20:48:09 -0400

    ensure directory + exhaust all pages

--- python/github_repo_cloner.py
+++ python/github_repo_cloner.py
@@ -4,13 +4,23 @@ import requests
 
 from git import Repo
 username = input("Please enter your Github account name: ")
-response = requests.get("https://api.github.com/users/" + username + "/repos?per_page=100")
+url = "https://api.github.com/users/" + username + "/repos?per_page=30"
+response = requests.get(url)
 repositories =  response.json()
-current_repos = os.listdir("./my_repositories/")
+try:
+    current_repos = os.listdir("./my_repositories/")
+except FileNotFoundError:
+    current_repos = []
 
-for repo in repositories:
-    if repo["name"] not in current_repos:
-        command = git.Git().clone(repo["clone_url"], "my_repositories/" + repo["name"], recursive=True)
-        r = Repo("my_repositories/" + repo["name"])
-        r.create_remote('originate', repo["clone_url"])
-        print(repo["name"])
+page = 1
+while len(repositories) > 0:
+    print('page ' + str(page))
+    page += 1
+    for repo in repositories:
+        if repo["name"] not in current_repos:
+            command = git.Git().clone(repo["clone_url"], "my_repositories/" + repo["name"], recursive=True)
+            r = Repo("my_repositories/" + repo["name"])
+            r.create_remote('originate', repo["clone_url"])
+            print(repo["name"])
+    new_response = requests.get(url + "&page=" + str(page))
+    repositories = new_response.json()
