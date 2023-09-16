8941778a3504d28191b58bb34afb15bde0ceb587
michael <michaelgdimmitt@gmail.com>
2023-09-16 14:24:42 -0400

    add working script with links

---
+++ bash/cloner.sh
@@ -0,0 +1,19 @@
+time (mkdir splunking; cd splunking; echo ; curl -s https://api.github.com/users/michaeldimmitt/repos | jq -c '.[]' | while read i; do val=$(jq -r '.clone_url' <<< "$i"); (git clone -q $val) &> /dev/null & done; echo -n loading; while echo -n .; ps e | grep -v grep | grep git > /dev/null; do sleep 1; done; echo ;)
+
+## Links
+
+# interogate json with jq
+# https://stackoverflow.com/questions/33950596/iterating-through-json-array-in-shell-script
+# echo "$res" | jq -c -r '.[]' | while read item; do     val=$(jq -r '.value' <<< "$item")     echo "Value: $val" done
+
+# wait for spawned processes doing git work to complete
+# https://stackoverflow.com/a/29822046/5283424
+# while ps axg | grep -vw grep | grep -w process_name > /dev/null; do sleep 1; done
+
+# call time on a bash function
+# https://unix.stackexchange.com/a/461813/188491
+# main () { echo running ... }; time main
+
+
+
+
